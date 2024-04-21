cdf424d506dd5c34c1c3005b7365d4e48c22c00b
Key Firdausi Alfarel <144311812+farelsukabelajar@users.noreply.github.com>
2024-04-21 15:22:08 +0700

    add empty constructor

--- F-BJV24001115-synrgy7-key-binfood-ch3/demo/src/main/java/synrgy/binfod/util/ErrorHandling.java
+++ F-BJV24001115-synrgy7-key-binfood-ch3/demo/src/main/java/synrgy/binfod/util/ErrorHandling.java
@@ -1,13 +1,25 @@
 package synrgy.binfod.util;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ErrorHandling {
+
+    private ErrorHandling() {
+
+    }
+
     private static final Logger logger = Logger.getLogger(ErrorHandling.class.getName());
 
     public static void handleException(Exception e) {
         logger.log(Level.SEVERE, "Terjadi kesalahan: " + e.getMessage(), e);
         System.err.println("Terjadi kesalahan: " + e.getMessage());
+    }
+
+    // Method untuk menangani IOException
+    public static void handleException(IOException e) {
+        logger.log(Level.SEVERE, "Terjadi kesalahan IO: " + e.getMessage(), e);
+        System.err.println("Terjadi kesalahan IO: " + e.getMessage());
     }
 }
