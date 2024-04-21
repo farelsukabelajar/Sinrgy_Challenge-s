4664fa9aa57e68639b8f4a2232447c2bba26770a
Key Firdausi Alfarel <144311812+farelsukabelajar@users.noreply.github.com>
2024-04-21 15:21:17 +0700

    add stream and lambda

--- F-BJV24001115-synrgy7-key-binfood-ch3/demo/src/main/java/synrgy/binfod/util/ReceiptUpdater.java
+++ F-BJV24001115-synrgy7-key-binfood-ch3/demo/src/main/java/synrgy/binfod/util/ReceiptUpdater.java
@@ -8,28 +8,33 @@ import synrgy.binfod.model.MenuItem;
 import synrgy.binfod.model.Order;
 
 public class ReceiptUpdater {
+
+    private ReceiptUpdater() {
+
+    }
+
     public static String generateReceipt(List<Order> orders) {
         StringBuilder receipt = new StringBuilder();
-        int totalItems = 0;
-        int totalPrice = 0;
-
+        int[] totalItems = { 0 };
+        int totalPrice = orders.stream()
+                .mapToInt(Order::getTotalPrice)
+                .sum();
         receipt.append("\n===========================================\n");
         receipt.append("             Struk Pembayaran             \n");
         receipt.append("===========================================\n\n");
         receipt.append(String.format("%-20s %-10s %-10s%n", "Menu", "Qty", "Harga"));
         receipt.append("\n");
 
-        for (Order order : orders) {
+        orders.forEach(order -> {
             MenuItem item = order.getItem();
             receipt.append(String.format("%-20s %-10d %-10s%n", item.getName(), order.getQuantity(),
                     "Rp. " + order.getTotalPrice()));
 
-            totalItems += order.getQuantity();
-            totalPrice += order.getTotalPrice();
-        }
+            totalItems[0] += order.getQuantity();
+        });
 
         receipt.append("------------------------------------------- +\n");
-        receipt.append(String.format("%n%-20s %-10d %-10s%n", "Total", totalItems, "Rp. " + totalPrice));
+        receipt.append(String.format("%n%-20s %-10d %-10s%n", "Total", totalItems[0], "Rp. " + totalPrice));
         receipt.append("\n\npembayaran     : BinarCash\n");
         receipt.append("\nDipesan pada   : " + getCurrentDateTime() + "\n");
         receipt.append("\n\n===========================================\n");
