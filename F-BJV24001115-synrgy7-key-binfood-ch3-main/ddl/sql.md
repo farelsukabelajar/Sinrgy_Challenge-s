d541f7a5d2f813f8a89cd59c7cce5d2149a215cb
Key Firdausi Alfarel <144311812+farelsukabelajar@users.noreply.github.com>
2024-04-23 23:08:36 +0700

    Delete F-BJV24001115-synrgy7-key-binfood-ch3-main directory

--- F-BJV24001115-synrgy7-key-binfood-ch3-main/ddl/sql.md
+++
@@ -1,1 +0,0 @@
-Note: "Database query menggunakan MySQL"
--- F-BJV24001115-synrgy7-key-binfood-ch3-main/src/main/java/org/example/view/OrderView.java
+++
@@ -1,7 +0,0 @@
-package org.example.view;
-
-public interface OrderView {
-    void displayMainMenu();
-    void displaySelectedMenu(String menuId, String menuName);
-    void displayOrderedMenu(String content);
-}
