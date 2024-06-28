d620c37d9f57c98cc52c0c837d1392a90685dd88
Key Firdausi Alfarel <144311812+farelsukabelajar@users.noreply.github.com>
2024-06-29 00:09:57 +0700

    Delete F-BJV24001115-synrgy7-key-binfood-ch5/binarfud/src/main/java/com/org/binarfud directory

--- F-BJV24001115-synrgy7-key-binfood-ch5/binarfud/src/main/java/com/org/binarfud/repo/OrderDetailRepo.java
+++
@@ -1,8 +0,0 @@
-package com.org.binarfud.repo;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.org.binarfud.model.OrderDetail;
-
-public interface OrderDetailRepo extends JpaRepository<OrderDetail, UUID> {
-}
